454977e799d61e5b8e56385dc537f8c08a0f252a
kuanoni <tehgamemaker@gmail.com>
2019-12-04 09:17:53 -0800

    added argparser and logging

--- StorytimeBot.py
+++ StorytimeBot.py
@@ -1,22 +1,48 @@
 import discord
 import string
 from collections import Counter
+import logging
+import argparse
 
-token = ""
+# Command line argument parser
+parser = argparse.ArgumentParser(description='Storytime Bot: a bot for moderating one-word-at-a-time stories')
+parser.add_argument("-t", required=True, default=0, help="Discord bot token")
+parser.add_argument("-s", required=True, default=0, type=int, help="Story channel id")
+parser.add_argument("-c", required=True, default=0, type=int, help="Command channel id")
+parser.add_argument("-p", default=3, type=int, help="People to post. (Default is 3)")
+parser.add_argument("-l", default=False, action='store_true', help="Log infractions?")
+args = parser.parse_args()
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter(fmt='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+
+token = args.t
 client = discord.Client()
 
-story_channel_id = 0
-command_channel_id = 0
+story_channel_id = args.s
+command_channel_id = args.c
 
 banned_words = []
 banned_users = []
-people_to_post = 3
-command_prefix = '!'
+people_to_post = args.p
 
 
 @client.event
 async def on_ready():
-    print(f"Bot has logged on as {client.user} and is ready to use.")
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
+    if args.l:
+        # create a file handler
+        handler = logging.FileHandler('discord.log')
+        handler.setLevel(logging.INFO)
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+    logger.info(f"Bot has logged on as {client.user} and is ready to use.")
 
 
 @client.event
@@ -26,6 +52,7 @@ async def on_message(msg):
         msg_text = msg.clean_content
 
         if check_banned(msg):
+            logger.warning(f"A banned user or message was posted: {msg.author.name} | {msg.clean_content}")
             await msg.delete()
 
         temp_users = []
@@ -33,9 +60,11 @@ async def on_message(msg):
             temp_users.append(message.author.name)
 
         if check_people_to_post(msg, temp_users):
+            logger.warning(f"{msg.author.name} attempted to multi-post: {msg.clean_content}")
             await msg.delete()
 
         if check_multiple_words(msg_text):
+            logger.warning(f"{msg.author.name} posted too many words: {msg.clean_content}")
             await msg.delete()
 
     # COMMANDS
@@ -44,11 +73,13 @@ async def on_message(msg):
         msg_text = msg.clean_content
 
         if msg_text == "!storytime":
+            logger.info(f"{msg.author.name} used !storytime")
             story = await get_story()
             for elem in [story[i:i + 2000] for i in range(0, len(story), 2000)]:  # discord messages can only be 2000 chars
                 await msg.channel.send(elem)
 
         if msg_text == "!storystats":
+            logger.info(f"{msg.author.name} used !storystats")
             stats = await get_stats()
             await msg.channel.send(stats)
 
