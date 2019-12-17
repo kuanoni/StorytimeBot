1179dbc05eb9d7e75da329348599c8575c078fe3
kuanoni <tehgamemaker@gmail.com>
2019-12-17 13:21:26 -0800

    changed to config file

--- StorytimeBot.py
+++ StorytimeBot.py
@@ -2,51 +2,57 @@ import discord
 import string
 from collections import Counter
 import logging
-import argparse
+import json
 
-# Command line argument parser
-parser = argparse.ArgumentParser(description='Storytime Bot: a bot for moderating one-word-at-a-time stories')
-parser.add_argument("-t", required=True, default=0, help="Discord bot token")
-parser.add_argument("-s", required=True, default=0, type=int, help="Story channel id")
-parser.add_argument("-c", required=True, default=0, type=int, help="Command channel id")
-parser.add_argument("-p", default=3, type=int, help="People to post. (Default is 3)")
-parser.add_argument("-l", default=False, action='store_true', help="Log infractions?")
-args = parser.parse_args()
+# load config file
+with open('config.json') as json_data_file:
+    config = json.load(json_data_file)
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter(fmt='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
 
-token = args.t
+# create logging handler
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+# create a file handler
+if config["log_to_file"]:
+    handler = logging.FileHandler('discord.log')
+    handler.setLevel(logging.INFO)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+token = config["token"]
 client = discord.Client()
 
-story_channel_id = args.s
-command_channel_id = args.c
+story_channel_id = config["story_channel_id"]
+command_channel_id = config["command_channel_id"]
 
 banned_words = []
+for word in config["banned_words"]:
+    banned_words.append(word)
+
 banned_users = []
-people_to_post = args.p
+for user in config["banned_users"]:
+    banned_words.append(user)
+
+people_to_post = config["people_to_post"]
 
 
 @client.event
 async def on_ready():
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.DEBUG)
-    ch.setFormatter(formatter)
-    logger.addHandler(ch)
-
-    if args.l:
-        # create a file handler
-        handler = logging.FileHandler('discord.log')
-        handler.setLevel(logging.INFO)
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
-
     logger.info(f"Bot has logged on as {client.user} and is ready to use.")
 
 
 @client.event
 async def on_message(msg):
+    if msg.channel.id == 631319628228067339:
+        if msg.clean_content == "!storysave":
+            await save_story()
+
     if msg.channel.id == story_channel_id:
         channel = msg.channel
         msg_text = msg.clean_content
@@ -75,7 +81,8 @@ async def on_message(msg):
         if msg_text == "!storytime":
             logger.info(f"{msg.author.name} used !storytime")
             story = await get_story()
-            for elem in [story[i:i + 2000] for i in range(0, len(story), 2000)]:  # discord messages can only be 2000 chars
+            for elem in [story[i:i + 2000] for i in
+                         range(0, len(story), 2000)]:  # discord messages can only be 2000 chars
                 await msg.channel.send(elem)
 
         if msg_text == "!storystats":
@@ -94,20 +101,19 @@ def check_banned(msg):
 
 
 def check_people_to_post(msg, history):
-    global people_to_post
     if history.count(msg.author.name) > 1:
         return True
 
 
 def check_multiple_words(msg):
-    msg = msg.translate(str.maketrans('', '', string.punctuation)) # remove punctuation
+    msg = msg.translate(str.maketrans('', '', string.punctuation))  # remove punctuation
     return len(msg.split()) > 1
 
 
 async def get_story():
     story_list = []
     channel = client.get_channel(story_channel_id)
-    async for msg in channel.history():
+    async for msg in channel.history(limit=100000):
         story_list.append(msg.clean_content)
 
     story_list.reverse()
@@ -115,7 +121,7 @@ async def get_story():
     for i in range(len(story_list)):
         story_list[i] = story_list[i].replace(' ', '')
 
-        if story_list[i][0] in string.punctuation:  # moves punctuation from beginning of word to end of previous word
+        if story_list[i][0] in '.,?!:;':  # moves punctuation from beginning of word to end of previous word
             story_list[i - 1] += (story_list[i][0])
             story_list[i] = story_list[i][1:]
 
@@ -140,9 +146,27 @@ async def get_stats():
 
     user_stats = "**@ Word Count Leaderboard @**\n"
     for i in range(len(user_list)):
-        user_stats = user_stats + user_list[i][0] + ": " + str(user_list[i][1]) + "\n"
+        user_stats += user_list[i][0] + ": " + str(user_list[i][1]) + "\n"
 
     return user_stats
+
+
+class Message:
+    def __init__(self, content, author):
+        self.content = content
+        self.author = author
+
+
+async def save_story():
+    history = []
+    channel = client.get_channel(631319628228067339)
+    async for msg in channel.history(limit=10000):
+        history.append(Message(msg.clean_content, msg.author.name))
+
+    json_string = json.dumps([message.__dict__ for message in history])
+    f = open("messages.json", "w")
+    f.write(json_string)
+    f.close()
 
 
 client.run(token)
